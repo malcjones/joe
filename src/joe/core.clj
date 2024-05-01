2f73c264cf0dff39bb4d9a073df92bc8f1895295
malcjones <33630760+malcjones@users.noreply.github.com>
2024-05-01 18:35:10 -0500

    docstrings!

--- src/joe/core.clj
+++ src/joe/core.clj
@@ -8,50 +8,67 @@
 
 (def bookmarks (atom ()))
 
-(defn create-bookmark [title url tags]
+(defn create-bookmark 
+  "Create a bookmark map with title, url, and tags."
+  [title url tags]
   {:title title :url url :tags tags})
 
-(defn listing [b]
+(defn listing 
+  "Format a bookmark for display."
+  [b]
   (str (:title b) " (" (blue (:url b)) ") [" (str/join ", " (map green (:tags b))) "]"))
 
-(defn load-bookmarks []
+(defn load-bookmarks
+  "Load bookmarks from file or create an empty list."
+  []
   (if (.exists (io/file "bookmarks.edn"))
     (reset! bookmarks (read-string (slurp "bookmarks.edn")))
     (println "no bookmarks found")))
 
-(defn save-bookmarks [formatter]
+(defn save-bookmarks 
+  "Save bookmarks to file using a formatter function."
+  [formatter] 
   (spit "bookmarks.edn" (formatter @bookmarks)))
+
+;; some formatters
 
 (def pretty-formatter (fn [b] (with-out-str (pprint b))))
 
 (def tiny-formatter (fn [b] (pr-str b)))
 
-(defn add-bookmark [title url & tags]
+(defn add-bookmark
+  "Add a bookmark to the list."
+  [title url & tags]
   (swap! bookmarks conj (apply create-bookmark title url tags))
   (println "added bookmark:" title))
 
-(defn list-bookmarks []
+(defn list-bookmarks 
+  "List all bookmarks."
+  []
   [] (doseq [bookmark @bookmarks]
        (println (listing bookmark))))
 
-(defn find-bookmarks [query]
+(defn find-bookmarks 
+  "Find bookmarks by title or tag."
+  [query]
   (filter (fn [b]
             (or
              (str/includes? (str/lower-case (:title b)) (str/lower-case query))
              (some #(str/includes? % query) (:tags b))))
           @bookmarks))
 
-(defn find-bookmark [query]
-  (first (find-bookmarks query)))
-
-(defn open-bookmark [query]
-  (if-let [b (find-bookmark query)]
+(defn open-bookmark 
+  "Open the first bookmark that matches the query."
+  [query]
+  (if-let [b (first (find-bookmarks query))]
     (do
       (.browse (Desktop/getDesktop) (URI. (:url b)))
       (println "opened bookmark:" (:title b)))
     (println "bookmark not found")))
 
 (defmulti command (fn [cmd & _] (keyword cmd)))
+
+;; command definitions
 
 (defmethod command :add [_ title url & tags]
   (add-bookmark title url tags))
